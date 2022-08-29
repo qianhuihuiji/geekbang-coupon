c234539118ed6ff8f4d9e619b3f03277ecb4e10b
meiyiming <1025926969@qq.com>
2022-08-29 22:31:51 +0800

    chapter 11

--- coupon-template-serv/coupon-template-impl/src/main/java/com/nofirst/coupon/template/CouponTemplateApplication.java
+++ coupon-template-serv/coupon-template-impl/src/main/java/com/nofirst/coupon/template/CouponTemplateApplication.java
@@ -2,20 +2,14 @@ package com.nofirst.coupon.template;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @ComponentScan(basePackages = {"com.nofirst"})
-@EnableTransactionManagement
-//用于扫描Dao @Repository
-@EnableJpaRepositories(basePackages = {"com.nofirst"})
-//用于扫描JPA实体类 @Entity，默认扫本包当下路径
-@EntityScan(basePackages = {"com.nofirst"})
+@EnableDiscoveryClient
 public class CouponTemplateApplication {
 
     public static void main(String[] args) {
